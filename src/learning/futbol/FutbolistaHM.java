51906edaa72cf90c3ab041e8bad1d7d74ab87a16
Ivan Uresti <iuresti@skytouchtechnology.com>
2020-03-05 08:55:45 -0600

    Add FutbolistaHM

---
+++ src/learning/futbol/FutbolistaHM.java
@@ -0,0 +1,27 @@
+package learning.futbol;
+
+public class FutbolistaHM extends SeleccionFutbolHM implements DeportistaInterface, IntegranteInterface {
+    private int dorsal;
+    private String demarcacion;
+
+    @Override
+    public void entrenar() {
+
+    }
+
+    @Override
+    public void jugarPartido() {
+
+    }
+
+
+    @Override
+    public void concentrarse() {
+
+    }
+
+    @Override
+    public void viajar() {
+
+    }
+}
